49651f5aecb4f65c825862ddc81c4ca2644ff7c3
budhathoki10 <budhathokikushal170@gmail.com>
2026-08-16 15:15:13 +0545

    docs: annotate auth.ts with explanatory comments

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,16 @@
+//“Allow GitHub login, save users and sessions in MongoDB, and include each user’s database ID in their session.”
+
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+// MongoDBAdapter: saves users, accounts, and sessions in MongoDB.
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
+//getMongoClient connects this project to MongoDB.
 import getMongoClient from "@/lib/mongodb";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: MongoDBAdapter(getMongoClient, { databaseName: process.env.MONGODB_DB }),
   providers: [GitHub],
+  
   session: { strategy: "database" },
   callbacks: {
     async session({ session, user }) {
